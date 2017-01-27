8cd56f5c437d841502e7635a119dec1091425de4
Ari Mindell <amindell11@gmail.com>
2017-01-27 09:40:51 -0500

    updated piston branch for new watcher system

--- Quick-Template-Ohm/src/org/usfirst/frc/team1389/robot/Robot.java
+++ Quick-Template-Ohm/src/org/usfirst/frc/team1389/robot/Robot.java
@@ -1,53 +1,51 @@
 package org.usfirst.frc.team1389.robot;
 
-import com.team1389.hardware.inputs.hardware.JoystickHardware;
+import java.util.concurrent.CompletableFuture;
 
+import com.team1389.hardware.inputs.hardware.JoystickHardware;
 import com.team1389.hardware.inputs.hardware.SwitchHardware;
 import com.team1389.hardware.inputs.software.DigitalIn;
-//import com.team1389.hardware.outputs.hardware.DoubleSolenoidHardware;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-
 import com.team1389.hardware.outputs.hardware.VictorHardware;
-import com.team1389.hardware.outputs.software.DigitalOut;
 import com.team1389.hardware.registry.Registry;
 import com.team1389.hardware.registry.port_types.DIO;
-import com.team1389.hardware.registry.port_types.PCM;
 import com.team1389.hardware.registry.port_types.PWM;
 import com.team1389.watch.Watcher;
 import com.team1389.watch.info.StringInfo;
 
+//import com.team1389.hardware.outputs.hardware.DoubleSolenoidHardware;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
 
 public class Robot extends IterativeRobot {
 	Watcher watcher = new Watcher();
 	Registry registry = new Registry();
-	
-	//Add hardware here
-	VictorHardware victor=new VictorHardware(false,new PWM(0),registry);
-	DoubleSolenoid piston= new  DoubleSolenoid(4,5); //(new PCM(4), new PCM(5), registry).getDigitalOut();
+
+	// Add hardware here
+	VictorHardware victor = new VictorHardware(false, new PWM(0), registry);
+	DoubleSolenoid piston = new DoubleSolenoid(4, 5); // (new PCM(4), new PCM(5),
+														// registry).getDigitalOut();
 	DigitalIn hallEffect = new SwitchHardware(new DIO(0), registry).getSwitchInput();
 
 	DigitalIn button = new JoystickHardware(0).getButton(2).getToggled();
-				
-	
+
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used for any
+	 * initialization code.
 	 */
 	@Override
 	public void robotInit() {
-		watcher.setLogLocation(Watcher.DEFAULT_LOG); //will overwrite the log file every time the code reboots
+		watcher.setLogLocation(Watcher.DEFAULT_LOG); // will overwrite the log file every time the
+														// code reboots
+		watcher.outputToDashboard();
+		CompletableFuture.runAsync(Watcher::updateWatchers);
 		watcher.watch(hallEffect.getWatchable("hallEffect"));
 		watcher.watch(button.getWatchable("button"));
-		//watcher.watch(piston.getWatchable("piston val"));
+		// watcher.watch(piston.getWatchable("piston val"));
 		watcher.watch(new StringInfo("PistonPosition", () -> piston.get().name()));
-	
-		}
 
-	
+	}
+
 	@Override
 	public void autonomousInit() {
 	}
@@ -64,9 +62,8 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		watcher.publish(Watcher.DASHBOARD);
-		piston.set(button.get()?Value.kForward: Value.kReverse);
-		}
+		piston.set(button.get() ? Value.kForward : Value.kReverse);
+	}
 
 	/**
 	 * This function is called periodically during test mode
@@ -74,7 +71,5 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void testPeriodic() {
 	}
-	
-	
 
 }
