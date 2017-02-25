d50e77d89d7e31749a9c4bbc24be999ea2cbd695
Ari Mindell <amindell11@gmail.com>
2017-02-24 23:00:39 -0500

    cleaned up bens camera code and got it working... still strange res bug

--- Quick-Template-Ohm/src/org/usfirst/frc/team1389/robot/Robot.java
+++ Quick-Template-Ohm/src/org/usfirst/frc/team1389/robot/Robot.java
@@ -1,44 +1,40 @@
 package org.usfirst.frc.team1389.robot;
 
-import java.util.concurrent.CompletableFuture;
-
 import com.team1389.hardware.inputs.hardware.JoystickHardware;
 import com.team1389.hardware.inputs.software.DigitalIn;
 import com.team1389.hardware.outputs.hardware.VictorHardware;
 import com.team1389.hardware.registry.Registry;
 import com.team1389.hardware.registry.port_types.PWM;
+import com.team1389.system.SystemManager;
 import com.team1389.watch.Watcher;
 
-import edu.wpi.cscore.MjpegServer;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
 
-public class Robot extends IterativeRobot{
+public class Robot extends IterativeRobot {
 	Watcher watcher = new Watcher();
 	Registry registry = new Registry();
-	SwitchingCameras switchingCameras = new SwitchingCameras();
-	
-	//Add hardware here
+
+	// Add hardware here
 	VictorHardware victor = new VictorHardware(false, new PWM(0), registry);
 	DigitalIn switchingCamerasButton = new JoystickHardware(0).getButton(2).latched();
-	
+	SwitchingCameras switchingCameras = new SwitchingCameras(switchingCamerasButton);
+	SystemManager manager = new SystemManager(switchingCameras);
+
 	/**
 	 * This function is run when the robot is first started up and should be used for any
 	 * initialization code.
 	 */
-	
+
 	@Override
 	public void robotInit() {
-		//CompletableFuture.runAsync(Watcher::updateWatchers);
-		//watcher.outputToDashboard();
-		switchingCameras.init();
+		manager.init();
+		watcher.watch(switchingCameras);
+		watcher.outputToDashboard();
 	}
 
 	@Override
 	public void autonomousInit() {
-		
+
 	}
 
 	/**
@@ -46,16 +42,18 @@ public class Robot extends IterativeRobot{
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		
+
 	}
 
 	/**
 	 * This function is called periodically during operator control
 	 */
-	
+
 	@Override
 	public void teleopPeriodic() {
+		Watcher.update();
 		switchingCameras.update();
+		manager.update();
 	}
 
 	/**
@@ -63,6 +61,6 @@ public class Robot extends IterativeRobot{
 	 */
 	@Override
 	public void testPeriodic() {
-		
+
 	}
 }
--- Quick-Template-Ohm/src/org/usfirst/frc/team1389/robot/SwitchingCameras.java
+++ Quick-Template-Ohm/src/org/usfirst/frc/team1389/robot/SwitchingCameras.java
@@ -1,41 +1,50 @@
 package org.usfirst.frc.team1389.robot;
 
 import com.team1389.hardware.inputs.software.DigitalIn;
+import com.team1389.system.Subsystem;
+import com.team1389.util.list.AddList;
+import com.team1389.watch.Watchable;
+import com.team1389.watch.info.StringInfo;
 
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
 
-public class SwitchingCameras{
+public class SwitchingCameras extends Subsystem {
 
 	DigitalIn switchingCamerasButton;
 	MjpegServer switcher;
-	UsbCamera camera0 = new UsbCamera("cam0", 0);
-	UsbCamera camera1 = new UsbCamera("cam1", 1);
-	UsbCamera CamAtMoment = camera0;
+	UsbCamera camera0;
+	UsbCamera camera1;
+	UsbCamera camAtMoment;
 
-	public void init(){
-		camera0 = CameraServer.getInstance().startAutomaticCapture();
-		camera1 = CameraServer.getInstance().startAutomaticCapture();
-		camera0.setResolution(640, 640);
-		camera1.setResolution(640, 640);
-
+	public SwitchingCameras(DigitalIn button) {
+		this.switchingCamerasButton = button;
 	}
-	
+
+	public void init() {
+		camera0 = new UsbCamera("camera 0", 0);
+		camera1 = new UsbCamera("camera 1", 1);
+		camera0.setResolution(640, 480);
+		camera1.setResolution(640, 480);
+
+		camAtMoment = camera0;
+		switcher = new MjpegServer("myserver", 5800);
+	}
+
 	public void update() {
-		while(true){
-			if(switchingCamerasButton.get()){
-				if(CamAtMoment == camera0){
-					//CamAtMoment = camera1;
-					switcher.setSource(camera1);
-				}
-			}
-			else{
-				//CamAtMoment = camera0;
-				switcher.setSource(camera0);
-			}
-			
+		if (switchingCamerasButton.get()) {
+			camAtMoment = camAtMoment == camera0 ? camera1 : camera0;
+			switcher.setSource(camAtMoment);
 		}
 	}
-}
 
+	@Override
+	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem) {
+		return stem.put(new StringInfo("selected cam", () -> camAtMoment.getName()));
+	}
+
+	@Override
+	public String getName() {
+		return "Camera Manager";
+	}
+}
